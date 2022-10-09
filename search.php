72f7a87ad80e564ecafb31aceee7dfdee00a4826
Vimukthi Lakshan <81724467+vimukthilakshan11@users.noreply.github.com>
2022-10-09 15:19:05 +0530

    search created

--- search.php
+++ search.php
@@ -1,3 +1,8 @@
+<?php
+    require_once ('./database/database.php');
+    $db = new dbConnect();
+
+?>
 <!doctype html>
 <html lang="en">
 
@@ -17,25 +22,98 @@
 
 <body>
 
+
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
     <?php 
            include 'common/header.php';
         ?> 
     </nav>
-
+<form method='POST' action='search.php'>
     <section id="search" class="container my-5 pt-5">
         <div class="search_wrap search_wrap_3">
             <div class="search_box">
-                <input type="text" class="input" placeholder="Search Here">
-                <div class="btn btn_common">
-                    <i class="fa fa-search white"></i>
-                </div>
+                <input type="text" class="input" placeholder="Search Here" name ='searchtext'>
+                
+                   
+                    <button name="search" class="btn btn_common"> <i class="fa fa-search white"></i></button>
+                    
+              
             </div>
         </div>
     </section>
+</form>
+
+<div class="row mx-auto container-fluid">
+           
+<?php
+
+if (isset($_POST["search"])) {
+
+        $str = $_POST["searchtext"];
+        $query = "SELECT lpa_stock_name,lpa_stock_ID,lpa_stock_price,lpa_stock_desc FROM lpa_stock Where lpa_stock_name = '$str'";
+    $result = $db->getfromdb($query);
+    $resultCheck = mysqli_num_rows($result);
+    
+        while ($row = mysqli_fetch_assoc($result)) {
+            $stockId = $row["lpa_stock_ID"];
+$amoun= $row["lpa_stock_price"];
+$disc= $row["lpa_stock_desc"];
 
 
+            $query2 = "SELECT lpa_stock_image FROM lpa_stock_image Where lpa_stock_ID = '$stockId'";
+            $result2 = $db->getfromdb($query2);
+            $resultCheck2 = mysqli_num_rows($result2);
+            
+                while ($row2 = mysqli_fetch_assoc($result2)) {
+                    $image = $row2["lpa_stock_image"];
+      
+                if($resultCheck2 > 0)
+                {
+                    ?>
+
+ <div class="product text-center col-lg-3 col-md-4 col-12">
+                <img class="img-fluid mb-3" src="admin/uploads/images/<?php echo $image;   ?>" alt="">
+
+                <div class="star">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+
+
+                </div>
+                <h5 class="p-name"><?php echo $disc ?></h5>
+                <h4 class="p-price"><?php echo $amoun ?></h4>
+                <button class="buy-btn">Add To Cart</button>
+            </div>
+           
+
+                 
+            <?php 
+                }
+                    
+                    
+                    else{
+                        
+                    }
+            
+            
+            }
+
+
+
+        }
+
+}
+
+
+
+?>
+
+            </div>
+    
     <footer class="mt-5 py-5">
     <?php   
          include 'common/footer.php';
