0c0bc6850fdcf3766fca02eb0591d02afd3f736e
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-10-11 01:07:42 +0530

    sidebar user image remove

--- admin/view/sidebar.php
+++ admin/view/sidebar.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION)) {
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="" class="img-circle elevation-2" alt="">
                     </div>
                     <div class="info">
                     <a href="index.php" class="d-block"><?php echo "<p class='d-block'>" . $_SESSION['sess_admin_username'] . "</p>";  ?></a>
