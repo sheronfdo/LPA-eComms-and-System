09fb8ea69d5136c3fe43e8094dcd98138e5f7592
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-10-11 01:03:13 +0530

    error report add

--- admin/view/logout.php
+++ admin/view/logout.php
@@ -1,4 +1,8 @@
 <?php  
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', $_SERVER['DOCUMENT_ROOT'] . '/lpa' . '/log.txt');
+error_reporting(E_ALL);
 require_once('../database/database.php');
 if(!isset($_SESSION)) 
     { 
